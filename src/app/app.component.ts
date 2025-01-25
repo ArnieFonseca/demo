6e9dc8fc5433031dc0039908d31ed02d73ff5963
Arnie  Fonseca <ArnieFonseca@hotmail.com>
2025-01-25 13:58:47 -0500

    Connect Client to Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CalculatorService } from './services/calculator.service'
+import { AdderComponent } from './components/adder/adder.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AdderComponent],
   templateUrl: './app.component.html'
   
 })
 export class AppComponent {
+
   title = 'Calculator';
 
-  firstNumber = "11"
-  secondNUmber  =  "7"
-  result = ""
 
-
-
-  constructor(private service:CalculatorService){}
-  
-  addNumber():void {
-
-    const fst  =  parseFloat(this.firstNumber)
-    const snd  =  parseFloat(this.secondNUmber)
-
-    const data  = this.service.getData(fst,snd)
-    data.subscribe({
-      next: (res: any) => {                      // When Data arrived
-        console.log("Result  from Service ", res)
-        this.result = res.result                                 // Display the data 
-      },
-      error: (err: any) => {                                 // When failure
-        console.log(err)
-    
-          }            // Display Server error   
-    })  
-
-
-
-
-  }
 }
--- src/app/services/calculator.service.ts
+++ src/app/services/calculator.service.ts
@@ -1,8 +1,8 @@
-// import { Injectable } from '@angular/core';
-// import { Observable, of } from 'rxjs';
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+ 
 
 
 @Injectable({
@@ -12,7 +12,7 @@ export class CalculatorService {
 
   constructor(private http: HttpClient) { }
 
-  getData(fst:number, snd:number): Observable<any> {
-    return this.http.get(`http://127.0.0.1:8000/calc/add/${fst}/${snd}`)
+  getData(oper:string, fst:number, snd:number): Observable<any> {
+    return this.http.get(`http://127.0.0.1:8000/calc/${oper}/${fst}/${snd}`)
   }
 }
